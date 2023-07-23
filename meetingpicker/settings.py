38d6c786e6b4fe1c83cbe20e827ac71adf55009b
Alec Zoeller <aleczoeller@gmail.com>
2023-07-23 15:56:17 +1200

    Added requirements.txt

--- meetingpicker/settings.py
+++ meetingpicker/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%bfcu=#juxx-e0b7r$o_jl0j&wi71cb%t=hf0s0d#azn8(t2#+'
+SECRET_KEY = os.getenv('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
