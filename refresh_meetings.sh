6a6befed120d49b63bec9931a6c68b8e6aa74b2c
Alec Zoeller <aleczoeller@gmail.com>
2023-09-04 18:25:18 +1200

    Advanced async functions to offload db calls to cronjob

---
+++ refresh_meetings.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+if [ ! -d ./data ]; then
+  mkdir -p data;
+fi
+
+declare -a arr_linux=("linux-gnu" "free-bsd" "darwin")
+for item in "${arr_linux[@]}}"
+do
+    if [ "$OSTYPE" = "$item" ]; then
+        ./refresh_meetings.py
+    fi
+done
+
+declare -a arr=("msys" "cygwin" "win32")
+for item in "${arr[@]}"
+do
+    if [ "$OSTYPE" = "$item" ] 
+        then ./refresh_meetings_win.py
+    fi
+done
